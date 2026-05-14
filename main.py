4f4d5c00e70bcfdf411cdf186387a9045237c5e0
Khun7874 <khunchitkhunlay.180@gmail.com>
2026-05-14 18:08:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-requirements.txt
-print("Hello World")
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8772657055:AAEbJjiDqBgySH48qlHzGJAIqcBSEcDPqVY
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot အလုပ်လုပ်နေပါပြီ ✅")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
